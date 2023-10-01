70e4e6d8385300f5cb1644dbe9f7a3132a223607
IvandroVicente <ivandrovicente9@gmail.com>
2023-10-01 14:05:05 -0400

    email atualizado

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
-function validateForm() {
-    var email = document.getElementById('email').value;
-    var receiverEmail = document.getElementById('receiverEmail').value;
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  
-    if (!email.match(emailRegex)) {
-      alert('Por favor, insira um e-mail válido.');
-      return false;
-    }
-  
-    if (!receiverEmail.match(emailRegex)) {
-      alert('Por favor, insira um e-mail de recebimento válido.');
-      return false;
-    }
-  
-    // Envie os dados para o servidor ou faça outras ações necessárias aqui
-    console.log('E-mail do remetente:', email);
-    console.log('E-mail de recebimento:', receiverEmail);
-  
-    // Limpe os campos após o envio (opcional)
-    document.getElementById('email').value = '';
-    document.getElementById('receiverEmail').value = '';
-  
-    return true;
-  }
-  
+function sendEmail(event) {
+  event.preventDefault();  // Prevents the default form submission
+
+  const nome = document.getElementById('nome').value;
+  const telefone = document.getElementById('email').value;
+  const email = document.getElementById('telefone').value;
+  const message = document.getElementById('assunto').value;
+
+  // You need to replace with your own email sending script
+  // Below is a placeholder for sending email using Gmail SMTP (you'd need a server backend for this)
+  window.open(`mailto:ivandroufr2020@gmail.com?subject=Contato&body=Nome:${nome} 
+  %0D%0A%0D%0AEmail:${email}
+  %0D%0A%0D%0ATelefone:${telefone}%0D%0A%0D%0AMensagem: ${message}`);
+
+  // Optionally, you can redirect to a thank you page or show a thank you message
+  alert('E-mail enviado com sucesso!');
+
+  // Clear the form
+  document.getElementById('email').value = '';
+  document.getElementById('message').value = '';
+}
