9657426c59196c0c5896965f3cec32a7ca9b4e66
IvandroVicente <ivandrovicente9@gmail.com>
2023-10-03 08:20:06 -0400

    Nova Atulalização

--- pages/About/script.js
+++ pages/About/script.js
@@ -3,3 +3,21 @@
 
 // Atualiza o elemento HTML com o ano atual
 document.getElementById('anoAtual').innerText = anoAtual;
+
+/*Dark Mode */
+
+function toggleDarkMode() {
+    const root = document.documentElement;
+    const lightIcon = document.getElementById('lightIcon');
+    const darkIcon = document.getElementById('darkIcon');
+  
+    if (root.classList.contains('dark')) {
+      root.classList.remove('dark');
+      lightIcon.classList.add('visible-icon');
+      darkIcon.classList.remove('visible-icon');
+    } else {
+      root.classList.add('dark');
+      lightIcon.classList.remove('visible-icon');
+      darkIcon.classList.add('visible-icon');
+    }
+  }
